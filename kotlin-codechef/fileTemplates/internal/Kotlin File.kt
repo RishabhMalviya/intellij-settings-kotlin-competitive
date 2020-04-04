70913cb2ae5dda277cecea6d93c80a322fedde92
Rishabh Malviya <rishabh.malviya@gmail.com>
2020-04-05 01:31:21 +0530

    add CodeChef settings

---
+++ kotlin-codechef/fileTemplates/internal/Kotlin File.kt
@@ -0,0 +1,44 @@
+import java.util.*
+
+
+private fun readLn() = readLine()!!
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+
+class ${NAME} {
+    fun solveTestCase(): Boolean {
+        //TODO: Solve the question
+    
+        return true
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = ${NAME}()
+
+    repeat((1..readInt()).count()) {
+        //TODO: Read in each Test Case
+    
+        println(solution.solveTestCase())
+    }
+}
